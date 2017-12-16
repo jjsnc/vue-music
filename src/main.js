228dbf2000bbae47ab946596f035024912aec5cd
jjsnc <zhuowangyi@163.com>
2017-12-16 08:20:10 +0800

    vue-lazyload 组件

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 import 'common/stylus/index.styl'
 
 // 去除移动端300ms 的延迟
 fastclick.attach(document.body)
-
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
