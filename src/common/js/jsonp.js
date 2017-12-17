7fa837c8accdc51dc73f8d43680b9928a656cd7d
jjsnc <zhuowangyi@163.com>
2017-12-17 15:06:30 +0800

    数据处理和singer 类封装

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -2,12 +2,13 @@
  * @Author: jjsnc
  * @Date:   2017-12-13 14:26:47
  * @Last Modified by:   jjsnc
- * @Last Modified time: 2017-12-13 15:21:25
+ * @Last Modified time: 2017-12-17 10:06:58
  */
 import originJSONP from 'jsonp'
 
 export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
       if (!err) {
@@ -20,11 +21,19 @@ export default function jsonp(url, data, option) {
 }
 
 // 把 data 拼接到url上
-function param(data) {
+// export function param(data) {
+//   let url = ''
+//   for (var k in data) {
+//     let value = data[k] !== undefined ? data[k] : ''
+//     url += `&$[k]=${encodeURIComponent(value)}`
+//   }
+//   return url ? url.substring(1) : ''
+// }
+export function param(data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
-    url += `&$[k]=${encodeURIComponent(value)}`
+    url += '&' + k + '=' + encodeURIComponent(value)
   }
   return url ? url.substring(1) : ''
 }
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,83 @@
 <template>
-	<div>
-	  歌手页面
-	</div>
+  <div>
+    歌手页面
+  </div>
 </template>
+<script>
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
 
-<script >
-	
+  const HOT_SINGER_LEN = 10
+  const HOT_NAME = '热门'
+  export default {
+    data() {
+      return {
+        singer: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          console.log(res)
+          if (res.code === ERR_OK) {
+            this.singer = res.data.list
+            this._normalizeSinger(res.data.list)
+          }
+        })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+        //  为了得到有序列表 我们需要处理map
+        let ret = []
+        let hot = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        return hot.concat(ret)
+      }
+    }
+  }
 </script>
-
-<style scoped lang="stylus" res='stylesheet/stylus'></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
